e0c5ae70093dc2b3f5b7fe360838647e1890c6d4
La Quốc Thắng <quocthang0507@gmail.com>
2021-03-12 21:47:51 +0700

    Update program

--- ColorPicker/MainForm.cs
+++ ColorPicker/MainForm.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ColorSystems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Windows.Forms;
 
@@ -6,47 +9,69 @@ namespace ColorPicker
 {
 	public partial class MainForm : Form
 	{
-		private TextBox textBox;
-		private TrackBar trackBar;
+		private RGBA RGBA;
+		private RGB RGB;
+		private CMYK CMYK;
+		private HSB HSB;
+		private HSL HSL;
 
 		public MainForm()
 		{
 			InitializeComponent();
+			new Thread(() => RegisterControls()).Start();
 		}
 
 		private void MainForm_Load(object sender, EventArgs e)
 		{
-			textBox = tbxRedGBA;
-			trackBar = trackRedGBA;
-			RegisterControls();
 		}
 
 		private void RegisterControls()
 		{
-			trackBar.ValueChanged += TrackBar_ValueChanged;
-			textBox.KeyPress += TextBox_KeyPress;
-			textBox.TextChanged += TextBox_TextChanged;
+			var trackBarControls = GetAll(this, typeof(TrackBar)).ToList();
+			var tbxControls = GetAll(this, typeof(TextBox)).ToList();
 
-			void TextBox_KeyPress(object sender, KeyPressEventArgs e)
+			foreach (TrackBar trackBar in trackBarControls)
 			{
-				if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
-					e.Handled = true;
-			}
+				var tbxName = trackBar.Name.Replace("track", "tbx");
+				var textBox = tbxControls.First(c => c.Name == tbxName);
+				if (textBox == null)
+					throw new NullReferenceException();
 
-			void TrackBar_ValueChanged(object sender, EventArgs e)
-			{
-				textBox.Text = trackBar.Value.ToString();
-			}
+				trackBar.ValueChanged += TrackBar_ValueChanged;
+				textBox.KeyPress += TextBox_KeyPress;
+				textBox.TextChanged += TextBox_TextChanged;
 
-			void TextBox_TextChanged(object sender, EventArgs e)
-			{
-				if (textBox.Focused)
+				void TextBox_KeyPress(object sender, KeyPressEventArgs e)
 				{
-					int value = Convert.ToInt32(textBox.Text);
-					trackBar.Value = value;
+					if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+						e.Handled = true;
 				}
+
+				void TrackBar_ValueChanged(object sender, EventArgs e)
+				{
+					textBox.Text = trackBar.Value.ToString();
+				}
+
+				void TextBox_TextChanged(object sender, EventArgs e)
+				{
+					if (textBox.Focused)
+					{
+						int value = Convert.ToInt32(textBox.Text);
+						trackBar.Value = value;
+					}
+				}
+
 			}
+
 		}
 
+		private IEnumerable<Control> GetAll(Control control, Type type)
+		{
+			var controls = control.Controls.Cast<Control>();
+
+			return controls.SelectMany(ctrl => GetAll(ctrl, type))
+									  .Concat(controls)
+									  .Where(c => c.GetType() == type);
+		}
 	}
 }
